7c4146e016e92daf83dd9648c67374fb4ca90a85
RamanaBhanuprakash <92294626+RamanaBhanuprakash@users.noreply.github.com>
2021-10-20 10:52:44 +0530

    Add files via upload

---
+++ INT FLOAT.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+//variable declaration
+int age;
+float cgpa;
+char initial;
+
+//initialization
+ age = 21;
+ cgpa = 8.21;
+ initial = 'B';
+ 
+ printf("age of the student is %d \n", age);
+ printf("cgpa of student is %f\n", cgpa);
+ printf("surname of student is %c\n", initial);
+}
